6abdec6b459e8ede53a1c4daa4f58837bc99a054
Junyong-Cho <junyong4510@gmail.com>
2026-03-05 17:54:06 +0900

    패킷 슬라이싱 기능 구현 클래스

---
+++ ServerCore/ServerCore/Sessions/PacketReadSession.cs
@@ -0,0 +1,19 @@
+﻿namespace ServerCore.Sessions;
+
+abstract class PacketReadSession : Session
+{
+    protected ArraySegment<byte>? SlicePacket(ref ArraySegment<byte> segment)
+    {
+        if (segment.Count < 2)
+            return null;
+
+        ushort size = BitConverter.ToUInt16(segment.Slice(0, 2));
+
+        if (segment.Count < size)
+            return null;
+
+        segment = segment.Slice(size);
+
+        return segment.Slice(0, size);
+    }
+}
